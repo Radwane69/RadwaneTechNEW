2f6b66ef9dcd178cb1576d8130d9abc4b1cfc65f
Radwane69 <rnougha@edenschool.fr>
2022-04-11 15:16:50 +0200

    commit content

--- js/app.js
+++ js/app.js
@@ -15,3 +15,11 @@ function controle(monform) {
     }
 }
 
+//On voit le resulat mais ne s'affiche pas longtemps
+function somme(){
+    var nbr1, nbr2, sum;
+    nbr1 = Number(document.getElementById("nbr1").value);
+    nbr2 = Number(document.getElementById("nbr2").value);
+    sum = nbr1 + nbr2;
+    document.getElementById("sum").value = sum;
+} 
